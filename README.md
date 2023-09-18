f1ac7fcd1c37f3e96cefbde64054a6a42fd353aa
johnferneymontoyaramirez <jfmontoya@ins.com.co>
2023-09-18 18:04:42 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Este es el ejercicio 22 correspondiente al capitulo 4
---
+++ src/main/java/john/ejercicio22cap4poo/Ejercicio22cap4poo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package john.ejercicio22cap4poo;
+
+/**
+ *
+ * @author johnferneymontoyaramirez
+ */
+public class Ejercicio22cap4poo {
+
+    public static void main(String[] args) {
+	    Form form = new Form();
+	    form.setVisible(true);
+    }
+}
